b9fce0727b0edc4b5587d98e7f27c390b8eba2c2
Ryucel <rustuyucel@protonmail.com>
2022-07-01 13:52:13 +0300

    Initial commit

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,14 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' hello
+#'
+#' @param name A name
+#'
+#' @return A message
+#' @export
+#'
+#' @examples hello("Alice")
 hello <- function(name){
   message <- paste0("Hello ", name, "!")
 
